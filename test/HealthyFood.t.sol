8461c94cb2d45953465938b83fed3ac7d1bd3ff6
akbarwijayaa <jimsaber02@gmail.com>
2024-10-03 12:45:39 +0700

    feat: add unit testing

--- test/HealthyFood.t.sol
+++ test/HealthyFood.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-import "../src/HealthyFood.sol"; // Path ke smart contract NFT Anda
+import "../src/HealthyFood.sol";
 
 contract HealthyFoodTest is Test {
     MyBaseNFT nft;
@@ -10,36 +10,34 @@ contract HealthyFoodTest is Test {
     address recipient;
 
     function setUp() public {
-        nft = new MyBaseNFT(); // Deploy kontrak
-        owner = address(this); // Owner adalah kontrak ini
-        recipient = vm.addr(1); // Buat alamat penerima baru
+        nft = new MyBaseNFT();
+        owner = address(this);
+        recipient = vm.addr(1);
     }
 
     function testMintNFT() public {
         string memory tokenURI = "https://example.com/metadata.json";
         uint256 newItemId = nft.mintNFT(recipient, tokenURI);
 
-        // Cek apakah NFT berhasil dimint
-        assertEq(newItemId, 1); // Karena ini NFT pertama yang dimint
-        assertEq(nft.ownerOf(newItemId), recipient); // Cek owner dari NFT yang dimint
-        assertEq(nft.tokenURI(newItemId), tokenURI); // Cek URI tokennya
+        assertEq(newItemId, 1);
+        assertEq(nft.ownerOf(newItemId), recipient);
+        assertEq(nft.tokenURI(newItemId), tokenURI);
     }
 
     function testTransferNFT() public {
         string memory tokenURI = "https://example.com/metadata.json";
-        uint256 newItemId = nft.mintNFT(owner, tokenURI); // Mint untuk owner
+        uint256 newItemId = nft.mintNFT(owner, tokenURI);
 
-        nft.transferNFT(owner, recipient, newItemId); // Transfer ke recipient
+        nft.transferNFT(owner, recipient, newItemId);
 
-        // Cek apakah NFT berhasil ditransfer
-        assertEq(nft.ownerOf(newItemId), recipient); // Owner seharusnya recipient sekarang
+        assertEq(nft.ownerOf(newItemId), recipient);
     }
 
     function testFailUnauthorizedTransfer() public {
         string memory tokenURI = "https://example.com/metadata.json";
-        uint256 newItemId = nft.mintNFT(recipient, tokenURI); // Mint untuk recipient
+        uint256 newItemId = nft.mintNFT(recipient, tokenURI);
 
-        vm.prank(address(2)); // Simulasikan panggilan dari address yang tidak berhak
-        nft.transferNFT(recipient, owner, newItemId); // Ini seharusnya gagal
+        vm.prank(address(2));
+        nft.transferNFT(recipient, owner, newItemId);
     }
 }
